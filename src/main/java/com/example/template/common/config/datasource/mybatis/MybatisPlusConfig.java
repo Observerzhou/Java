c5b9f47cf95acef6e446324978f71501f6a854e2
Observerzhou <18260621997@163.com>
2022-08-29 00:40:30 +0800

    2022/08/29 zhouhuihui 集成动态数据源

---
+++ src/main/java/com/example/template/common/config/datasource/mybatis/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.example.template.common.config.datasource.mybatis;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * <p> MybatisPlus配置类 </p>
+ *
+ * @author zhouhuihui
+ * @description
+ * @date 2022/08/29
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.example.template.**.mapper*")
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：https://mp.baomidou.com/guide/page.html <br>
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
